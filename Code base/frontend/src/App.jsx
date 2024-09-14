a9bb56e3182638c1922df38e654e6cf14e3dffbc
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2024-09-14 09:30:14 +0700

    Update App.jsx

--- Code base/frontend/src/App.jsx
+++ Code base/frontend/src/App.jsx
@@ -1,16 +1,4 @@
-import React, { useState } from "react";
-import Navbar from "./components/navbar/navbar";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/home";
-import Cart from "./pages/cart/cart";
-import Placeorder from "./pages/placeorder/placeorder";
-import Footer from "./components/Footer/Footer";
-import LoginPopup from "./components/LoginPopup/LoginPopup";
-
-const App = () => {
-  const[showLogin,setShowLogin]= useState(false)
-  return (
-      <div className="app">
+     <div className="app">
         {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
@@ -20,7 +8,3 @@ const App = () => {
         </Routes>
         <Footer />
         </div>
-  );
-};
-
-export default App;
