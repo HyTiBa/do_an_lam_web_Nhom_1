602220eb721eb5a75a3ea4437d76ef2488234e93
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2024-09-17 17:07:06 +0700

    explore menu filter, food item add/remove from cart

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const menu_links = document.querySelectorAll(".navbar-menu a");
+const food_item_counter = document.querySelector(".counter");
 const explore_menu_list = document.querySelector(".explore-menu-list");
+const food_display_list = document.querySelector(".food-display-list");
 let explore_menu_category = "all";
+
+const cart_items = {};
+
+const food_list = [
+  {
+    id: 1,
+    name: "Cơm chiên dương châu",
+    image: "./images/com chien duong chau.jpg",
+    price: 35000,
+    description:
+      "Là món cơm chiên Việt Nam hấp dẫn với tôm, xúc xích, trứng và rau, được nêm nếm hoàn hảo để tạo nên một bữa ăn đậm đà hương vị.",
+    category: "Cơm chiên",
+  },
+  {
+    id: 2,
+    name: "Bún bò",
+    image: "./images/bun bo.jpg",
+    price: 35000,
+    description:
+      "Là một ăn đậm đà của Việt Nam với thịt bò mềm, bún và các loại thảo mộc thơm trong nước dùng cay nồng.",
+    category: "Bún",
+  },
+  {
+    id: 3,
+    name: "Lẩu gà",
+    image: "./images/lau ga.webp",
+    price: 80000,
+    description:
+      "món lẩu gà của Việt Nam, gồm thịt gà mềm, rau và thảo mộc được ninh trong nước dùng đậm đà, ăn kèm với mì hoặc cơm.",
+    category: "Lẩu",
+  },
+];
 
 const menu_list = [
   {
@@ -69,6 +103,7 @@ explore_menu_list_items.forEach((item) => {
       );
       explore_menu_category = "all";
     }
+    load_food_items_display();
   });
 
   item_p.addEventListener("click", () => {
@@ -83,6 +118,77 @@ explore_menu_list_items.forEach((item) => {
         image.classList.remove("active")
       );
       explore_menu_category = "all";
+      load_food_items_display();
     }
   });
 });
+
+load_food_items_display();
+
+function load_food_items_display() {
+  food_display_list.innerHTML = "";
+
+  food_list.forEach((food) => {
+    food_display_list.innerHTML +=
+      explore_menu_category == "all" || explore_menu_category == food.category
+        ? `
+    <div class="food-item">
+            <div  class="food-item-img-container">
+              <img src='${food.image}' class="food-item-img"/>
+                <div class="counter">
+                  ${counter_logic(food.id)}
+                </div>
+            </div>
+            <div class="food-item-info">
+              <div class="food-item-name-rating">
+                <p>${food.name}</p>
+                <img src='./images/1-star.png' alt="" />
+              </div>
+              <p class="food-item-description">${food.description}</p>
+              <p class="food-item-price">${food.price}đ</p>
+            </div>
+          </div>
+      `
+        : "";
+  });
+  add_remove_icon();
+}
+
+function counter_logic(id) {
+  if (cart_items[id] > 0) {
+    return `
+    <img id='${id}' class="icon subtract minus-icon" src="./images/red minus icon.png"/>
+    <p>${cart_items[id]}</p>
+    <img id='${id}' class="icon add plus-icon" src="./images/green plus icon.png" alt=""/>
+`;
+  } else {
+    return `<img id='${id}' class="plus-icon icon" src="./images/plus icon.png"/>`;
+  }
+}
+
+function add_remove_icon() {
+  let plus_icon = document.querySelectorAll(".plus-icon");
+  let minus_icon = document.querySelectorAll(".minus-icon");
+  plus_icon.forEach((element) =>
+    element.addEventListener("click", () => add_to_cart(element.id))
+  );
+  minus_icon.forEach((element) =>
+    element.addEventListener("click", () => remove_from_cart(element.id))
+  );
+}
+
+function add_to_cart(id) {
+  if (!cart_items[id]) {
+    cart_items[id] = 1;
+  } else {
+    cart_items[id] += 1;
+  }
+  load_food_items_display();
+  console.log(cart_items);
+}
+function remove_from_cart(id) {
+  cart_items[id] -= 1;
+  console.log(cart_items);
+
+  load_food_items_display();
+}
