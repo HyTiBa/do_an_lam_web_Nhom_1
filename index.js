b77dc4b02115d2805c511167e5c57739d8cf4c66
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2024-11-04 10:37:28 +0700

    setting pages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { menuListDisplay } from "./modules/menuList.js";
-import { foodListDisplay } from "./modules/foodList.js";
+import { menuListDisplay } from "./modules/menuList.js"
+import { foodListDisplay } from "./modules/foodList.js"
+import { pageDisplay } from "./modules/pageDisplay.js"
+let pageToDisplay = "home"
 
-
+pageDisplay(pageToDisplay)
 menuListDisplay()
 foodListDisplay()
---
+++ modules/pageDisplay.js
@@ -0,0 +1,8 @@
+const pageButtonLinks = document.querySelectorAll(".admin-navbar-nav a")
+const pages = document.querySelectorAll(".page")
+
+
+export function pageDisplay(pageToDisplay){
+console.log("hello");
+
+}
