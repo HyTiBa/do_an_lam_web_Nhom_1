a4e86bbe784f6a3d2974ea4279e012e387d319a6
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2024-11-05 07:52:40 +0700

    PageDisplay Done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import { menuListDisplay } from "./modules/menuList.js"
 import { foodListDisplay } from "./modules/foodList.js"
 import { pageDisplay } from "./modules/pageDisplay.js"
-let pageToDisplay = "home"
 
 
 
-pageDisplay(pageToDisplay)
+pageDisplay()
 menuListDisplay()
 foodListDisplay()
--- modules/pageDisplay.js
+++ modules/pageDisplay.js
@@ -1,13 +1,38 @@
 const pageButtonLinks = document.querySelectorAll(".admin-navbar-nav a")
 const pages = document.querySelectorAll(".page")
+let pageToDisplay
+pages.forEach(page => {
+    if(page.attributes.page.value == "home"){
+        pageToDisplay = page
+        
+    }
+})
 
-
-export function pageDisplay(pageToDisplay){
-    pages.forEach(page => console.log(page.attributes.page.value)
-
+export function pageDisplay(){
+    removeAllPages()
+    displayChoosenPage(pageToDisplay)
+    
+    pageButtonLinks.forEach(link => 
+        link.addEventListener("click", () => displayChoosenPage(link)
+        )
     )
 }
 
 function removeAllPages(){
+    pages.forEach(page => page.style.display = "none"
+    )
+}
 
+
+function displayChoosenPage(pageLink){
+
+    removeAllPages()
+    pages.forEach(page => {
+        if(getPageValue(page) == getPageValue(pageLink)){
+            page.style.display = "block"
+    }})
+}
+
+function getPageValue(page){
+    return page.attributes.page.value
 }
