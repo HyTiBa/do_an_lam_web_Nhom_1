ccd0fe658a8f4de398ad349d431dbc39a08fa1db
KaztYuu <92777635+KaztYuu@users.noreply.github.com>
2024-11-08 18:54:00 +0700

    Update
    Chac the

--- index.js
+++ index.js
@@ -193,6 +193,8 @@ function remove_from_cart(id) {
   load_food_items_display();
 }
 
+
+//Sign code
 document.querySelector(".login-btn").addEventListener("click",() => {
   document.querySelector(".popup-box").style.display = "flex";
 })
@@ -213,8 +215,48 @@ document.querySelector(".close-form").addEventListener("click",() => {
   document.querySelector(".popup-box").style.display = "none"
 })
 
-const user = {
-  email: '',
-  userName:'',
-  password:''
+const userArr = [];
+const productArr = [];
+
+function checkPwd(P1, P2){
+  if(P1 !== P2){
+    return false;
+  }
+  return true;
 }
+function checkEmail(userE){
+  return true;
+}
+
+function addUser(event){
+  event.preventDefault();
+  let userE = document.getElementsByName('email')[0].value;
+  let userN = document.getElementsByName('fname')[0].value;
+  let userP1 = document.getElementsByName('password')[0].value;
+  let userP2 = document.getElementsByName('confirm_password')[0].value;
+  if(!checkEmail){
+    alert('Tai khoan Gmail da ton tai');
+    return 0;
+  }
+  if(checkPwd(userP1, userP2)){
+    const user = {
+      email: userE,
+      userName: userN,
+      password: userP1,
+      roll: 'Khach hang'
+    };
+    userArr.push(user);
+  }
+  else{
+    alert('Mat khau nhap lai khong trung khop');
+    document.getElementsByName('password')[0].value ="";
+    document.getElementsByName('confirm_password')[0].value = "";
+  }
+}
+
+function loginAcc(event){
+  event.preventDefault();
+}
+
+document.getElementById('signup_form').addEventListener('submit', addUser);
+document.getElementById('signin_form').addEventListener('submit', loginAcc);
