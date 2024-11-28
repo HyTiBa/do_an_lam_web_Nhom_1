9c5c87844de15a944e07790dcb0afe64cc7be7fe
KaztYuu <92777635+KaztYuu@users.noreply.github.com>
2024-11-29 06:26:40 +0700

    Khanh

--- modules/adminUserManage.js
+++ modules/adminUserManage.js
@@ -1,6 +1,7 @@
 import { users } from "./informationalObjects.js";
 import { setLocalStorage } from "./informationalObjects.js";
 import { adminUserBoardDisplay } from "./adminUserBoard.js";
+import { isPasswordValid } from "./signCode.js";
 
 export function adminUserManage(){
 
@@ -11,88 +12,112 @@ export function adminUserManage(){
             let userN = document.querySelector('.adminUserBoard .pop-ups .modify #modify_name').value;
             let userP = document.querySelector('.adminUserBoard .pop-ups .modify #modify_pwd').value;
             let userR = document.querySelector('.adminUserBoard .pop-ups .modify #modify_role').value;
-
-            for(let i = 0; i < users.length; i++){
-                if(userED === users[i].email){
-                    users[i].userName = userN;
-                    users[i].password = userP;
-                    users[i].roles = userR;
-                    break;
+            if(isPasswordValid(userP)){
+                for(let i = 0; i < users.length; i++){
+                    if(userED === users[i].email){
+                        users[i].userName = userN;
+                        users[i].password = userP;
+                        users[i].roles = userR;
+                        break;
+                    }
                 }
+                alert('Cập nhật người dùng thành công');
+                document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'none';
+                document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
+                document.querySelector('.adminUserPage .top-section .adminUserSearch .search-box input').value = '';
+                adminUserBoardDisplay();
+                reattachEventListenerAdminUserBoard();
+            } else{
+                alert('Mật khẩu phải có ít nhất 6 ký tự');
+                document.querySelector('.adminUserBoard .pop-ups .modify #modify_pwd').value ='';
             }
-            alert('Cập nhật người dùng thành công');
-            document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'none';
-            document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
-            document.querySelector('.adminUserPage .top-section .adminUserSearch .search-box input').value = '';
-            adminUserBoardDisplay();
-            reattachEventListenerAdminUserBoard();
         }
     }
 
     function reattachEventListenerAdminUserBoard(){
         document.querySelectorAll(".adminUserBoard .actions .modify").forEach((button) => {
-            button.addEventListener('click', (event) =>{
-                document.querySelector(".adminUserBoard .pop-ups").style.display = 'flex';
-                document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'flex';
-                userED = event.target.closest(".user").querySelector(".info .email").textContent;
-                for(let i = 0; i < users.length; i++){
-                    if(userED === users[i].email){
-                        document.querySelector('.adminUserBoard .pop-ups .modify #modify_name').value = users[i].userName;
-                        document.querySelector('.adminUserBoard .pop-ups .modify #modify_pwd').value = users[i].password;
-                        document.querySelector('.adminUserBoard .pop-ups .modify #modify_role').value = users[i].roles;
-                        break;
-                    }
-                }
-            })
-        })
-    
-        if(document.querySelector('.adminUserBoard .pop-ups .modify .form-container')){
-            document.querySelector('.adminUserBoard .pop-ups .modify .form-container').addEventListener('submit', (event) =>{
-                event.preventDefault();
-                updateUser();
-             })
-        }
-
-        if(document.querySelector(".adminUserBoard .btn-close")){
-            document.querySelector(".adminUserBoard .btn-close").addEventListener('click', () =>{
-                document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'none';
-                document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
-            })
-        }
-
-        document.querySelectorAll(".adminUserBoard .actions .remove").forEach((button) => {
-            button.addEventListener('click', (event) =>{
-                document.querySelector(".adminUserBoard .pop-ups").style.display = 'flex';
-                document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'block';
-                userED = event.target.closest(".user").querySelector(".info .email").textContent;
-            })
-        
-        })
-
-        if(document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn')){
-            document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn').addEventListener('click', () =>{
-                if(userED != null){
+            if(!button.hasAttribute('data-listener')){
+                button.addEventListener('click', (event) =>{
+                    document.querySelector(".adminUserBoard .pop-ups").style.display = 'flex';
+                    document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'flex';
+                    userED = event.target.closest(".user").querySelector(".info .email").textContent;
                     for(let i = 0; i < users.length; i++){
                         if(userED === users[i].email){
-                            users.splice(i, 1);
+                            document.querySelector('.adminUserBoard .pop-ups .modify #modify_name').value = users[i].userName;
+                            document.querySelector('.adminUserBoard .pop-ups .modify #modify_pwd').value = users[i].password;
+                            document.querySelector('.adminUserBoard .pop-ups .modify #modify_role').value = users[i].roles;
                             break;
                         }
                     }
-                    alert('Xóa người dùng thành công');
-                    document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'none';
+                })
+                button.setAttribute('data-listener', 'true');
+            }
+        })
+    
+        if(document.querySelector('.adminUserBoard .pop-ups .modify .form-container')){
+            if(!document.querySelector('.adminUserBoard .pop-ups .modify .form-container').hasAttribute('data-listener')){
+                document.querySelector('.adminUserBoard .pop-ups .modify .form-container').addEventListener('submit', (event) =>{
+                    event.preventDefault();
+                    updateUser();
+                })
+                document.querySelector('.adminUserBoard .pop-ups .modify .form-container').setAttribute('data-listener', 'true');
+            }
+        }
+
+        if(document.querySelector(".adminUserBoard .btn-close")){
+
+            if(!document.querySelector(".adminUserBoard .btn-close").hasAttribute('data-listener')){
+                document.querySelector(".adminUserBoard .btn-close").addEventListener('click', () =>{
+                    document.querySelector(".adminUserBoard .pop-ups .modify").style.display = 'none';
                     document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
-                    document.querySelector('.adminUserPage .top-section .adminUserSearch .search-box input').value = '';
-                    adminUserBoardDisplay();
-                    reattachEventListenerAdminUserBoard();
-                }
-            })
+                })
+                document.querySelector(".adminUserBoard .btn-close").setAttribute('data-listener', 'true');
+            }
+        }
+
+        document.querySelectorAll(".adminUserBoard .actions .remove").forEach((button) => {
+            if(!button.hasAttribute('data-listener')){
+                button.addEventListener('click', (event) =>{
+                    document.querySelector(".adminUserBoard .pop-ups").style.display = 'flex';
+                    document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'block';
+                    userED = event.target.closest(".user").querySelector(".info .email").textContent;
+                })
+                button.setAttribute('data-listener', 'true');
+            }
+        })
+
+        if(document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn')){
+
+            if(!document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn').hasAttribute('data-listener')){
+                document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn').addEventListener('click', () =>{
+                    if(userED != null){
+                        for(let i = 0; i < users.length; i++){
+                            if(userED === users[i].email){
+                                users.splice(i, 1);
+                                break;
+                            }
+                        }
+                        alert('Xóa người dùng thành công');
+                        document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'none';
+                        document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
+                        document.querySelector('.adminUserPage .top-section .adminUserSearch .search-box input').value = '';
+                        adminUserBoardDisplay();
+                        reattachEventListenerAdminUserBoard();
+                    }
+                })
+                document.querySelector('.adminUserBoard .pop-ups .remove .cfm_btn').setAttribute('data-listener', 'true');
+            }
         }
 
         if(document.querySelector(".adminUserBoard .deny_btn")){
-            document.querySelector(".adminUserBoard .deny_btn").addEventListener('click', () => {
-                document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'none';
-                document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
-            })
+
+            if(!document.querySelector(".adminUserBoard .deny_btn").hasAttribute('data-listener')){
+                document.querySelector(".adminUserBoard .deny_btn").addEventListener('click', () => {
+                    document.querySelector(".adminUserBoard .pop-ups .remove").style.display = 'none';
+                    document.querySelector(".adminUserBoard .pop-ups").style.display = 'none';
+                })
+                document.querySelector(".adminUserBoard .deny_btn").setAttribute('data-listener', 'true');
+            }
         }
     }
     function checkEmail(userE){
@@ -136,21 +161,30 @@ export function adminUserManage(){
     
     reattachEventListenerAdminUserBoard();
 
-    document.querySelector('.adminUserPage .top-section .addUser_btn').addEventListener('click', () =>{
-        document.querySelector('.adminUserPage .top-section .pop-ups').style.display = 'flex';
-    })
+    if(!document.querySelector('.adminUserPage .top-section .addUser_btn').hasAttribute('data-listener')){
+        document.querySelector('.adminUserPage .top-section .addUser_btn').addEventListener('click', () =>{
+            document.querySelector('.adminUserPage .top-section .pop-ups').style.display = 'flex';
+        })
+        document.querySelector('.adminUserPage .top-section .addUser_btn').setAttribute('data-listener', 'true');
+    }
+   
+    if(!document.querySelector('.adminUserPage .top-section .pop-ups .add-user .btn-close').hasAttribute('data-listener')){
+        document.querySelector('.adminUserPage .top-section .pop-ups .add-user .btn-close').addEventListener('click', () =>{
+            document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_email').value ='';
+            document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_name').value ='';
+            document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_pwd').value ='';
+            document.querySelector('.adminUserPage .top-section .pop-ups').style.display = 'none';
+        })
+        document.querySelector('.adminUserPage .top-section .pop-ups .add-user .btn-close').setAttribute('data-listener', 'true');
+    }
 
-    document.querySelector('.adminUserPage .top-section .pop-ups .add-user .btn-close').addEventListener('click', () =>{
-        document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_email').value ='';
-        document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_name').value ='';
-        document.querySelector('.adminUserPage .top-section .pop-ups .add-user #added_pwd').value ='';
-        document.querySelector('.adminUserPage .top-section .pop-ups').style.display = 'none';
-    })
-
-    document.querySelector('.adminUserPage .top-section .add-user .form-container').addEventListener('submit', (event) =>{
-        event.preventDefault();
-        adminAddUser();
-    })
+    if(!document.querySelector('.adminUserPage .top-section .add-user .form-container').hasAttribute('data-listener')){
+        document.querySelector('.adminUserPage .top-section .add-user .form-container').addEventListener('submit', (event) =>{
+            event.preventDefault();
+            adminAddUser();
+        })
+        document.querySelector('.adminUserPage .top-section .add-user .form-container').setAttribute('data-listener', 'true');
+    }
 
     document.querySelector('.adminUserPage .top-section .adminUserSearch .search-box input').addEventListener('input', (event) =>{
         let userArr = [];
